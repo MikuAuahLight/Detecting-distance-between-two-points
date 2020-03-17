cad099f0fcf23ddf6fc26732ea85f8db08e0434c
MikuAuahLight <62160791+MikuAuahLight@users.noreply.github.com>
2020-03-17 10:08:02 -0700

    Update function_dist.lua
    The Function

--- function_dist.lua
+++ function_dist.lua
@@ -1,3 +1,22 @@
-Distance = function(x1, y1, x2, y2)
+-- Local Function
+
+local _Distance = function(x1, y1, x2, y2)
     return math.floor(math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2)))
 end
+
+-- [[
+
+-- If you want to make it a global function, Change "status" value from 0 to 1
+
+local status = 0
+
+do
+   if (status == 1) then
+       function Distance(...)
+           return Distance(...)
+       end
+       status = nil
+    end
+end
+
+]]
